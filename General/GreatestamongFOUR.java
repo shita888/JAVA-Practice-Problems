e2170875bb5ef4c7bd347af787b343a02dc4ddc0
shita888 <sheetalnarwariya40@gmail.com>
2026-05-11 19:56:41 +0530

    new

---
+++ General/GreatestamongFOUR.java
@@ -0,0 +1,22 @@
+public class GreatestamongFOUR { 
+    public static void main(String[] args) {
+        System.out.println("Greatest among three...."); 
+        
+        int a=2,b=3,c=4,d=5; 
+        if(a>b && a>c && a>d){
+            System.out.println("a="+a);
+        }else if(b>c && b>d){
+            System.out.println("b="+b);
+        }else if(c>d){
+             System.out.println("c="+c);
+        }else{
+             System.out.println("d="+d);
+        }
+        
+        System.out.println( "Equiliteral Triangle....");
+        
+        
+        
+    }
+
+}
