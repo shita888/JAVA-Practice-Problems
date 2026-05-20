e879c751d9b1367566a65ff79b15192626591a78
shita888 <sheetalnarwariya40@gmail.com>
2026-05-20 19:05:21 +0530

    Small_Projects

---
+++ Arrays_Practise/Evenmultiple.java
@@ -0,0 +1,15 @@
+package Arrays_Practise;
+
+public class Evenmultiple {
+    public static void main(String[] args) {
+        // int arr[] = {4,1,6,0,24,9,7,12,21};
+
+        // for (int n : arr){    
+        //     if(n%3==0 && n%2==0){
+        //         System.out.println(n);
+        //     }      
+        // }
+
+        
+    }
+}
