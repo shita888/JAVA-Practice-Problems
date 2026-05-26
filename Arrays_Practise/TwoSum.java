6edf96e6529ab58b41e3a8835fe70e91299a6dd4
shita888 <sheetalnarwariya40@gmail.com>
2026-05-26 21:42:45 +0530

    new program

--- Arrays_Practise/TwoSum.java
+++ Arrays_Practise/TwoSum.java
@@ -1,26 +1,43 @@
 public class TwoSum {
+    
     public static void main(String[] args) {
-         int arr[] = {1,2,3,1,3,1}; 
-           System.out.println("Array:");
+        
+         int arr[] = {1,2,3,2,1}; 
+         
+         System.out.println("Array:");
          for(int i=0;i<arr.length;i++){
              System.out.print(arr[i]+" ");
          }
          
-          System.out.println("\nTwo Sum:");
-          int target=1;
-          for(int i=0;i<arr.length;i++)
-          {        
-              for(int j=i+1;j<=arr.length-1;j++){
-                  if(arr[i]+arr[j]==target){
-                    //   System.out.println(arr[i]+" "+arr[j]+": "+target);
-                      System.out.println("Yes");
-                       break;
-                  }
-              }   
-            
+         int brr[] = new int[arr.length];
+         for(int i=0;i<brr.length;i++){
+             brr[i]=arr[i];
+         }
+          System.out.println("\nBrray:");
+         for(int i=brr.length-1;i>=0;i--){
+             System.out.print(brr[i]+" ");
          }
          
          
+          System.out.println("\nCheck the Array:");
+          int target=1 ;
+          for(int i=brr.length-1;i>=0;i--)
+          {        
+              for(int j=0;j<brr.length;j++)
+              {
+                  if(arr[i]==arr[j]){
+                      target=1; 
+                  }else{
+                     target++; 
+                  }
+              }   
+          }
+          
+          if( target==1){
+                      System.out.println( "Palindrome");  
+                  }else{
+                     System.out.println( "NOT Palindrome");  
+                  }
          
     }
 }
