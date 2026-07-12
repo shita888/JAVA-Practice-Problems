f091bb8cc4e06518d0c022c21e0febbb3e6adcfc
shita888 <sheetalnarwariya40@gmail.com>
2026-07-13 00:28:15 +0530

    new program

--- Strings/ps6.java
+++ Strings/ps6.java
@@ -1,3 +1,77 @@
-public class ps6 {
-    
+//......Leetcode 989 problem
+public class ps6 { 
+
+    public static int addToArrayForm(int[] nums, int k) {
+        int n =0,cn=0,ck=0,k1=k,rem2;
+        //......
+        while(k1>0){
+           rem2 = k1%10;
+           ck++;; 
+           k1=k1/10;
+        } 
+        //,.......
+        for(int i=0;i<nums.length;i++){ 
+            n= (n*10)+nums[i];
+        }
+        System.out.println("Int:"+n);
+        
+//.........        
+        int k2=n;
+         while(k2>0){
+           rem2 = k2%10;
+           cn++;; 
+           k2=k2/10;
+        } 
+//..........   
+        n = n+k;
+        System.out.println("Add:"+n); 
+        
+        int rem,rem1 ,rev=0;
+        while(n>0){
+           rem = n%10;
+           rev = (rev*10)+rem;
+           n=n/10;
+        }
+         System.out.println("Rev:"+rev);
+         
+        System.out.println();
+        System.out.println("ele:"+ck);
+        System.out.println("ele:"+cn);
+        int m = rev;
+        if(ck>cn){
+             int nums1[] = new int[ck];
+                     int i=0;
+                    while(m>0){
+                        rem1 = m%10; 
+                        nums1[i] = rem1;
+                        i++;
+                        m=m/10;
+                    }
+                    System.out.println("Array:");
+                    for(int j=0;j<nums1.length;j++){ 
+                        System.out.print(nums1[j]+" ");
+                    }   
+        }else{
+                       int i=0; 
+                        while(m>0){
+                        rem1 = m%10; 
+                         nums[i] = rem1;
+                         i++;
+                         m=m/10;
+                        }
+        
+                        System.out.println("Array:");
+                        for(int j=0;j<nums.length;j++){ 
+                        System.out.print(nums[j]+" ");
+                        }   
+            
+        }
+        
+        return m;
+    }
+    public static void main(String args[]){  
+        int num[]={1,2,0,0},k=10000;
+        addToArrayForm(num,k);
+    }
 }
+
