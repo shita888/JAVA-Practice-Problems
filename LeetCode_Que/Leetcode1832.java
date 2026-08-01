52fe450a9127f722c1db4dd45de77f198035bf1e
shita888 <sheetalnarwariya40@gmail.com>
2026-08-01 23:56:27 +0530

    Leetcode que

--- LeetCode_Que/Leetcode1832.java
+++ LeetCode_Que/Leetcode1832.java
@@ -3,27 +3,39 @@ package LeetCode_Que;
 public class Leetcode1832 {
     // 97 - 122
     public static boolean checkIfPangram(String sentence) {
-      
-        //  for(char i=97;i<=122;i++){ 
-        char i = 97;
-            System.out.println(sentence.contains("i")0);
-        //  }
+        String n = "123"; 
  
-  
-
-    //  if(n==0){
-    //     System.out.println("True");
-    //     return true;
-    //  }else{
-    //     System.out.println("False");
-        return false;
-    //  }
+        return false; 
 
     }
     public static void main(String[] args) {
-        String sentence = "the quck";
+        // String sentence = "the quck";
         // String sentence = "thequickbrownfoxjumpsoverthelazydog";
-        checkIfPangram(sentence);
+        // checkIfPangram(sentence);
+
+        int x=8;
+        int p=x;
+         int n=1 ;
+         while(p>0){
+             p = p/2;
+             if(p==1){
+                 break;
+                }
+                n++;
+            } 
+            
+        if(n%2==0){ 
+             double result = x/2;
+            System.out.println(result);
+            int k = (int)result;
+            return k;
+        }else{
+            double result = x/4;
+            result = result*1.414;
+            System.out.println(result);
+            int k = (int)result;
+            return k;
+        }
 
     }
 }
